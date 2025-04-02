4af61f3c9abae20510343d52bd4c2919a92d78f5
Stroyoes <121812287+Stroyoes@users.noreply.github.com>
2025-04-02 07:30:28 +0530

    Update `pptr_elms.h`

--- elms_using_ptrs/pptr_elms.h
+++ elms_using_ptrs/pptr_elms.h
@@ -2,7 +2,7 @@
 #define PRINT_PTR_ELMS
 
 void ptr_print_elms(int *arr, int size_of_arr);
-int get_sum_using_ptrs(int *arr, int size_of_arr);
+void get_sum_using_ptrs(int *arr, int size_of_arr);
 
 #endif
 
