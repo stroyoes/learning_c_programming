36631495bce3d2757b39d3200015a09f03d7004d
Stroyoes <mazky7804@gmail.com>
2025-04-27 12:34:15 +0530

    Update list using C

--- list_using_c/clist.c
+++ list_using_c/clist.c
@@ -14,12 +14,52 @@ clist_t* create_clist() {
   clist->size = 0;
   clist->capacity = 4;
 
-  clist->buffer = calloc(clist->capacity, sizeof(int)); 
+  clist->buffer = calloc(clist->capacity, sizeof(int));
+
+  if (clist->buffer == NULL) {
+    clist_error = CLIST_BUFFER_ALLOC_ERR;
+    return CLIST_ERR_NULL;
+  }
 
   return clist;
 }
 
-void append(clist_t *list, int value) {
+CLIST_RETURN_NOTHING append(clist_t *clist, int value) {
+  if (clist->size >= clist->capacity) {
+    clist->capacity *= 2;
+    clist->buffer = realloc(clist->buffer, clist->capacity * sizeof(int));
+
+    if (clist->buffer == NULL) {
+      clist_error = CLIST_BUFFER_ALLOC_ERR;
+      return ;
+    }
+
+  }
+  clist->buffer[clist->size++] = value;
+}
+
+int get(clist_t *clist, int index) {
+  if (index < 0) index += clist->size;
+  if (index < 0 || index >= clist->size) {
+    
+    clist_error = CLIST_INDEX_ERR;
+    return CLIST_UNBOUND_INDEX;
+
+  }
+
+  return clist->buffer[index];
+}
+
+int contains(clist_t *clist, int value) {
+  for (int i = 0; i < clist->size; i++) {
+    if (clist->buffer[i] == value) {
+      return true; 
+    }
+  }
+  return false;
+}
+
+int count(clist_t *clist, int value) {
 
 }
 
--- list_using_c/clist.h
+++ list_using_c/clist.h
@@ -1,6 +1,11 @@
 #ifndef CLIST_H
 #define CLIST_H
 
+#include <stdbool.h>
+
+#define CLIST_RETURN_NOTHING void
+#define CLIST_UNBOUND_INDEX -1
+
 typedef struct {
   int *buffer;
   int size;
